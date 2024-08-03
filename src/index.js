780fe0a82a3677acb3a7dd5a82c7be04f846571e
Sergey <sergeyproshko2005@gmail.com>
2024-08-03 15:57:50 +0300

    feat: full task complited!

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 module.exports = function reverse (n) {
+    let modul = n*(-1)
+    if (n < 0){
+        return String(modul).split('').reverse().join('')
+    }
     return String(n).split('').reverse().join('')
 }
